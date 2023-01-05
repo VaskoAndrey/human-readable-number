d7d60992f81d68a2076f0b02eeac88f36fe7be52
AndreyVasko <nate20@mail.ru>
2023-01-05 20:44:58 +0300

    feat: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function toReadable (number) {
-  
+  let dunits = ['zero','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen','eighteen', 'nineteen',];
+  let dozens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety',];
+
+
 }
